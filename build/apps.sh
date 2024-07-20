d26cf4c9a035c9de2b28805b8f3e27394875a4c3
bean980310 <bean980310@hotmail.co.kr>
2024-07-20 11:13:27 +0900

    Add nodejs

--- build/apps.sh
+++ build/apps.sh
@@ -35,5 +35,10 @@ rclone selfupdate
 
 curl https://getcroc.schollz.com | bash
 
+curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
+./nodesource_setup.sh 
+
+apt install -y nodejs
+
 # curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | bash && \
 #     apt install -y speedtest
