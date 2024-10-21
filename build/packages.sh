801dac2a55672ea5e065054441d8839ee0285880
bean980310 <bean980310@hotmail.co.kr>
2024-10-21 15:45:56 +0900

    Update packages.sh

--- build/packages.sh
+++ build/packages.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
-apt update && apt -y full-upgrade
-apt install -y \
+set -e
+
+apt-get update
+apt-get install -y \
     build-essential \
+    pandoc \
+    gcc \
+    g++ \
     software-properties-common \
     python3-tk \
     python3-dev \
     python3-launchpadlib \
-    nodejs \
-    npm \
     bash \
     git \
     git-lfs \
-    ncdu \
     net-tools \
+    netcat-openbsd \
     openssh-server \
     libglib2.0-0 \
     libsm6 \
@@ -22,32 +25,17 @@ apt install -y \
     ffmpeg \
     wget \
     curl \
-    psmisc \
-    rsync \
-    vim \
-    zip \
-    unzip \
     p7zip-full \
-    htop \
-    screen \
-    tmux \
-    bc \
-    aria2 \
-    cron \
     pkg-config \
-    plocate \
     libcairo2-dev \
     libgoogle-perftools4 \
     libtcmalloc-minimal4 \
-    apt-transport-https \
     ca-certificates \
-    pandoc \
-    gcc \
     jq
 update-ca-certificates
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash -
-sudo apt-get install -y nodejs
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+apt-get install -y nodejs
 npm install -g npm@latest
-apt clean
+apt-get clean
 rm -rf /var/lib/apt/lists/*
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
